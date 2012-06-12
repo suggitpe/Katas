96f104eefe3a064bb2a64f430582c5e11109e7d3
Peter Suggitt <me@suggs.org.uk>
2012-06-12 07:32:10 +0100

    moved away from gradle until it matures

---
+++ 1-novice/nasa-countdown/src/main/java/org/suggs/katas/novice/nasacountdown/NasaCountdown.java
@@ -0,0 +1,12 @@
+package org.suggs.katas.novice.nasacountdown;
+
+/**
+ * User: suggitpe
+ * Date: 11/06/12
+ * Time: 07:36
+ */
+public class NasaCountdown {
+    public Integer[] countdown(int aStartingNumber) {
+        return new Integer[]{0};
+    }
+}
--- 1-novice/nasa-countdown/src/test/java/org/suggs/katas/novice/nasacountdown/NasaCountdownStepdefs.java
+++ 1-novice/nasa-countdown/src/test/java/org/suggs/katas/novice/nasacountdown/NasaCountdownStepdefs.java
@@ -1,5 +1,12 @@
 package org.suggs.katas.novice.nasacountdown;
 
+import cucumber.annotation.en.Then;
+import cucumber.annotation.en.When;
+import cucumber.runtime.PendingException;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 /**
  * User: suggitpe
  * Date: 11/06/12
@@ -7,4 +14,27 @@ package org.suggs.katas.novice.nasacountdown;
  */
 @SuppressWarnings("unused")
 public class NasaCountdownStepdefs {
+
+    private NasaCountdown countdown = new NasaCountdown();
+    private Integer[] countdownSequence;
+
+
+    @When("^I request a countdown from (\\d+)$")
+    public void I_request_a_countdown_from(int aStartingNumber) throws Throwable {
+        countdownSequence = countdown.countdown(aStartingNumber);
+    }
+
+    @Then("^I expect an array with (\\d+) numbers$")
+    public void I_expect_an_array_with_numbers(int totalNumberInCountdownSequence) throws Throwable {
+        assertThat(countdownSequence.length, is(totalNumberInCountdownSequence));
+    }
+
+    @Then("^the array countdown in order from (\\d+) to (\\d+)$")
+    public void the_array_countdown_in_order_from_to(int aBiggestNumber, int aSmallestNumber) throws Throwable {
+        assertThat(countdownSequence[0], is(aBiggestNumber));
+        assertThat(countdownSequence[countdownSequence.length], is(aSmallestNumber));
+
+    }
+
+
 }
